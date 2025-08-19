b2c9d219ac518b84a1e9159bf33747b1e9c68b3d
Eden Abwe <edenjardin089@gmail.com>
2025-08-18 23:34:26 -0400

    Building an API for creating issues

---
+++ prisma/client.ts
@@ -0,0 +1,16 @@
+// src/lib/prismadb.ts
+import { PrismaClient } from "@prisma/client";
+
+const prismaClientSingleton = () => new PrismaClient();
+
+declare const globalThis: {
+  prismaGlobal: ReturnType<typeof prismaClientSingleton>;
+} & typeof global;
+
+const prisma = globalThis.prismaGlobal ?? prismaClientSingleton();
+
+if (process.env.NODE_ENV !== "production") {
+  globalThis.prismaGlobal = prisma;
+}
+
+export default prisma;
