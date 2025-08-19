a92fee87e331d278524ea9b1db370d973ceb1860
Eden Abwe <edenjardin089@gmail.com>
2025-08-18 23:45:32 -0400

    Set up Radix UI

--- app/issues/page.tsx
+++ app/issues/page.tsx
@@ -1,7 +1,12 @@
 import React from "react";
+import { Button } from "@radix-ui/themes";
 
 const issuesPage = () => {
-  return <div>issuePages</div>;
+  return (
+    <div>
+      <Button>New Issue</Button>
+    </div>
+  );
 };
 
 export default issuesPage;
