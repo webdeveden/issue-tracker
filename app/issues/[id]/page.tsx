fc121ee55038bbd87870ecfc66772449378accc2
Eden Abwe <edenjardin089@gmail.com>
2025-08-21 20:18:57 -0400

    Add additional loading skeleton

--- app/issues/[id]/page.tsx
+++ app/issues/[id]/page.tsx
@@ -4,6 +4,7 @@ import { Card, Flex, Heading, Text } from "@radix-ui/themes";
 import { notFound } from "next/navigation";
 import React from "react";
 import ReactMarkdown from "react-markdown";
+import delay from "delay";
 
 interface Props {
   params: { id: string };
@@ -15,6 +16,9 @@ const IssueDetailPage = async ({ params }: Props) => {
   });
 
   if (!issue) notFound();
+
+  await delay(6000);
+
   return (
     <>
       <Heading>{issue.title}</Heading>
--- app/issues/new/loading.tsx
+++ app/issues/new/loading.tsx
@@ -1,7 +1,16 @@
+import { Box } from "@radix-ui/themes";
 import React from "react";
+import Skeleton from "react-loading-skeleton";
+import "react-loading-skeleton/dist/skeleton.css";
 
 const LoadingNewIssuePage = () => {
-  return <div>Loading...</div>;
+  return (
+    <Box className="max-w-xl ">
+      <Skeleton />
+      <Skeleton height="20rem" />
+      <Skeleton width="10rem" />
+    </Box>
+  );
 };
 
 export default LoadingNewIssuePage;
