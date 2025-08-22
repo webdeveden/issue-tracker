14b236740028f02e104d9fca5ac574d89f22561b
Eden Abwe <edenjardin089@gmail.com>
2025-08-21 21:21:35 -0400

    Add the edit button

---
+++ app/issues/[id]/edit/page.tsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+const EditPage = () => {
+  return <div>Edit your page here .enjoy</div>;
+};
+
+export default EditPage;
--- app/issues/[id]/page.tsx
+++ app/issues/[id]/page.tsx
@@ -1,10 +1,12 @@
 import IssuesStatusBadge from "@/app/components/IssuesStatusBadge";
 import prisma from "@/prisma/client";
-import { Card, Flex, Heading, Text } from "@radix-ui/themes";
+import { Box, Button, Card, Flex, Grid, Heading, Text } from "@radix-ui/themes";
 import { notFound } from "next/navigation";
 import React from "react";
 import ReactMarkdown from "react-markdown";
-import delay from "delay";
+import { Pencil2Icon } from "@radix-ui/react-icons";
+import Link from "next/link";
+// import delay from "delay";
 
 interface Props {
   params: { id: string };
@@ -17,19 +19,28 @@ const IssueDetailPage = async ({ params }: Props) => {
 
   if (!issue) notFound();
 
-  await delay(6000);
+  //   await delay(6000);
 
   return (
-    <>
-      <Heading>{issue.title}</Heading>
-      <Flex gap="3" my="2">
-        <IssuesStatusBadge status={issue.status} />
-        <Text>{issue.createdAt.toDateString()}</Text>
-      </Flex>
-      <Card className="prose mt-4">
-        <ReactMarkdown>{issue.description}</ReactMarkdown>
-      </Card>
-    </>
+    //column size based on devices
+    <Grid columns={{ initial: "1", md: "2" }} gap="5">
+      <Box>
+        <Heading>{issue.title}</Heading>
+        <Flex gap="3" my="2">
+          <IssuesStatusBadge status={issue.status} />
+          <Text>{issue.createdAt.toDateString()}</Text>
+        </Flex>
+        <Card className="prose mt-4">
+          <ReactMarkdown>{issue.description}</ReactMarkdown>
+        </Card>
+      </Box>
+      <Box>
+        <Button>
+          <Pencil2Icon />
+          <Link href={`/issues/${issue.id}/edit`}> Edit Issue</Link>
+        </Button>
+      </Box>
+    </Grid>
   );
 };
 
